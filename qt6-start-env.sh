8238bf9faab9a16556c99a4aa84c9cd71af5c919
Adam Pigg <adam@piggz.co.uk>
2026-07-25 20:56:42 +0100

    Make the main qt6 profile env source the user version.
    This will allow the user to exec binaries without using qt6-start from a
    terminal

--- qt6-start-env.sh
+++ qt6-start-env.sh
@@ -4,3 +4,14 @@ QT_QPA_PLATFORMTHEME=kde
 QT_QUICK_CONTROLS_MOBILE=1
 QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 QT_MEDIA_BACKEND=gstreamer
+
+if [ -f "${QT6_ENV}" ] ; then
+        echo Overriding environment with user specified values
+        source "${QT6_ENV}"
+elif [ -f "${XDG_CONFIG_HOME}"/qt6-start.env ] ; then
+        echo Overriding environment with user specified values
+        source "${XDG_CONFIG_HOME}"/qt6-start.env
+elif [ -f "${HOME}"/.config/qt6-start.env ] ; then
+        echo Overriding environment with user specified values
+        source "${HOME}"/.config/qt6-start.env
+fi
--- qt6-start.sh
+++ qt6-start.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-#Source the global fiel first
+#Source the qt6 environment
 source /etc/profile.d/qt6-start-env.sh
-
-if [ -f "${QT6_ENV}" ] ; then
-        echo Overriding environment with user specified values
-        source "${QT6_ENV}"
-elif [ -f "${XDG_CONFIG_HOME}"/qt6-start.env ] ; then
-        echo Overriding environment with user specified values
-        source "${XDG_CONFIG_HOME}"/qt6-start.env
-elif [ -f "${HOME}"/.config/qt6-start.env ] ; then
-        echo Overriding environment with user specified values
-        source "${HOME}"/.config/qt6-start.env
-fi
 
 program=$1
 shift || true
